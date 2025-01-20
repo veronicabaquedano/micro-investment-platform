4f224cfe04bfa78b8e24fa299b37114a3a89c511
veronicabaquedano <baquedano2407@hotmail.com>
2025-01-20 14:46:06 +0100

    start user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class MyUser(AbstractBaseUser):
+    email = models.CharField(max_length=150, unique=True)
+    ...
+    USERNAME_FIELD = "email"
