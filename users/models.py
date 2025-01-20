a51a1042da5629dfecda1cb9ecc0ce7bdb62ca7d
veronicabaquedano <baquedano2407@hotmail.com>
2025-01-20 16:49:18 +0100

    finish user model

--- users/models.py
+++ users/models.py
@@ -7,15 +7,17 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
         if not email:
             raise ValueError("Users must have an email address")
+        # new user instance
         user = self.model(
             email=self.normalize_email(email),
-        )  # new user instance
+        )  
         user.set_password(password)  # hash and set password
         user.save(using=self._db)  # save user to db
         return user
 
     # creates and saves admin users
     def create_superuser(self, email, password=None):
+        # Call create_user
         user = self.create_user(
             email,
             password=password,
@@ -26,8 +28,14 @@ class UserManager(BaseUserManager):
 
 
 class MyUser(AbstractBaseUser):
-    email = models.CharField(max_length=150, unique=True)
-    is_active = models.BooleanField(default=True)
-    # set_password(raw_password)
-    # check_password(raw_password)
-    USERNAME_FIELD = "email"
+        # Unique identifier for users
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True) # Indicates if the user is active
+    is_admin = models.BooleanField(default=False) #indicates if user is admin
+    objects = UserManager()  # Assign UserManager to the User model
+    USERNAME_FIELD = "email"   # Use email as the unique identifier
+
