c950879f0306284fc260e1c86919eaeda0ffa6b8
veronicabaquedano <baquedano2407@hotmail.com>
2025-01-22 13:35:36 +0100

    work on serializers and migrations

--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-#creates users
+
+# creates users
 class UserManager(BaseUserManager):
     # creates and saves regular users.
     def create_user(self, email, password=None):
@@ -10,7 +11,7 @@ class UserManager(BaseUserManager):
         # new user instance
         user = self.model(
             email=self.normalize_email(email),
-        )  
+        )
         user.set_password(password)  # hash and set password
         user.save(using=self._db)  # save user to db
         return user
@@ -23,19 +24,48 @@ class UserManager(BaseUserManager):
             password=password,
         )
         user.is_admin = True
+        user.is_staff = True  # Allow access to admin
         user.save(using=self._db)
         return user
 
-#save user to database/ represents user structure in db
+
+# save user to database/ represents user structure in db
 class User(AbstractBaseUser):
-        # Unique identifier for users
+    # Unique identifier for users
     email = models.EmailField(
         verbose_name="email address",
         max_length=255,
         unique=True,
     )
-    is_active = models.BooleanField(default=True) # Indicates if the user is active
-    is_admin = models.BooleanField(default=False) #indicates if user is admin
-    objects = UserManager()  # Assign UserManager to the User model
-    USERNAME_FIELD = "email"   # Use email as the unique identifier
+    is_active = models.BooleanField(default=True)  # Indicates if the user is active
+    is_admin = models.BooleanField(default=False)  # indicates if user is admin
+    is_staff = models.BooleanField(default=False)  # Admin access
 
+    # Add more fields like first_name, last_name if needed
+    objects = UserManager()  # Assign UserManager to the User model
+    USERNAME_FIELD = "email"  # Use email as the unique identifier
+    REQUIRED_FIELDS = []
+
+    # returns email for readability
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        if self.is_admin: 
+            return True
+        return False
+
+    def has_module_perms(self, app_label):
+        if self.is_admin: 
+            return True
+        return False
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
---
+++ users/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from users.models import User
+
+#validates incoming data and interacts with model/ maps model fields to serializer fields
+#(e.g., email is required, password is not blank).
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    class Meta:
+        model = User
+        #fields to include in the serialized data
+        fields = ['email', 'password']
+        #write-only, so won’t be returned in API responses
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+#handles creation of new user      
+#validated_data, contains the validated email and password from the API request.  
+def create(self, validated_data):
+    # Uses the create_user method from the UserManager to create and save the user 
+    # and makes sure password is hashed
+    return User.objects.create_user(**validated_data)
+        
