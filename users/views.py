e94c8e49f9318725c5bd40e3a101c9984440e60d
veronicabaquedano <baquedano2407@hotmail.com>
2025-01-22 14:56:50 +0100

    view registration logic

--- users/views.py
+++ users/views.py
@@ -1,21 +1,25 @@
-from rest_framework.views import APIView #class-based view provided by Django Rest Framework (DRF) for building APIs.
-from rest_framework.response import Response #Used to return structured responses (e.g., JSON).
-from rest_framework import status #Provides HTTP status codes for responses (e.g., 201 Created, 400 Bad Request).
+from rest_framework.views import (
+    APIView,
+)  # class-based view provided by Django Rest Framework (DRF) for building APIs.
+from rest_framework.response import (
+    Response,
+)  # Used to return structured responses (e.g., JSON).
+from rest_framework import (
+    status,
+)  # Provides HTTP status codes for responses (e.g., 201 Created, 400 Bad Request).
 from users.serializers import UserSerializer
 
 
-#handles user registration. can handle POST and GET because of APIView
+# handles user registration. can handle POST and GET because of APIView
 class UserRegistrationView(APIView):
-    #send data to the server /sensitive info
+    # send data to the server /sensitive info
     def post(self, request):
-        #contains the data sent in the API request
+        # contains the data sent in the API request
         # data is passed to the UserSerializer for validation
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(    {"message": "User created successfully."}, 
-    status=status.HTTP_201_CREATED)
-        
-        
-   
-
+            return Response(
+                {"message": "User created successfully."},
+                status=status.HTTP_201_CREATED,
+            )
