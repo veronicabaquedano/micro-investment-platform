bbcfb867d4e3c2a235db5c6f719ad1e8fe610c61
veronicabaquedano <baquedano2407@hotmail.com>
2025-01-23 15:32:40 +0100

    start transactions model

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings  # refer to user model even if it changes
 
-# Create your models here.
+
+# represents structure of transaction table in db
+class Transactions(models.Model):
+    # establish connection between transaction and user tables.
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="transactions"
+    )
+    #store monetary value of transaction
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    #when transaction is created
+    timestamp = models.DateTimeField(auto_now_add=True)
+    #metadata for model
+    class Meta:
+        #order by timestamp descending
+        ordering = ["-timestamp"]
