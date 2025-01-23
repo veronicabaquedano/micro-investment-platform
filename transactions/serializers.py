6d908d14e783d6af66f5721d49c97dc36b01f774
veronicabaquedano <baquedano2407@hotmail.com>
2025-01-23 17:30:35 +0100

    add serializer for transaction

---
+++ transactions/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Transaction
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ["id", "user", "amount", "timestamp"]  # Include all fields
+        read_only_fields = ["id", "timestamp"]  # `id` and `timestamp` are auto-generated
