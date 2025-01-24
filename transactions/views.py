2259d6a14643bcbeb92d7a3122090730ff30c0a6
veronicabaquedano <baquedano2407@hotmail.com>
2025-01-24 08:16:58 +0100

    add views for transactions

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .models import Transaction
+from .serializers import TransactionSerializer
 
-# Create your views here.
+class TransactionListCreateView(APIView):
+    permission_classes = [IsAuthenticated]  # User must be logged in
+
+    def get(self, request):
+        """List all transactions for the logged-in user."""
+        transactions = Transaction.objects.filter(user=request.user) 
+        #uses serializer to convert data into JSON
+        serializer = TransactionSerializer(transactions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        """Create a new transaction for the logged-in user."""
+        #validate and process data provided by user
+        serializer = TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)  # Attach the logged-in user, saves in db
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
