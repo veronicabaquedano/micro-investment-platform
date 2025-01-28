bd03a5d7f6f5184646aad7d3366a9e1c7fba42b2
veronicabaquedano <baquedano2407@hotmail.com>
2025-01-28 15:59:36 +0100

    make migrations for savings

--- savings/signals.py
+++ savings/signals.py
@@ -1,7 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
-from .models import Transaction, Savings
+from transactions.models import Transaction
+from .models import Savings
 from decimal import Decimal
 
 
