b6b6fa537c460879107ebe4b3d770daf4e6507ec
veronicabaquedano <baquedano2407@hotmail.com>
2025-01-28 15:03:11 +0100

    create savings model

--- savings/models.py
+++ savings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Savings(models.Model):
+    # Saving ID is automatically created as "id" by Django.
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name="savings"
+    )  # User ID links to the user model. It references the id field in the User model.
+    total_savings = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  # Total amount
+    last_updated = models.DateTimeField(auto_now=True)  # Last updated timestamp
+
+    def __str__(self):
+        return f"Savings for {self.user.email} - ${self.total_savings}"
