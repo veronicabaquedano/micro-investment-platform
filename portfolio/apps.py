71be46c01a8342accc837298577da6160be1bf36
veronicabaquedano <baquedano2407@hotmail.com>
2025-02-03 11:29:03 +0100

    change porfolio name and migrate models

--- portfolio/apps.py
+++ portfolio/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PorfolioConfig(AppConfig):
+class PortfolioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'porfolio'
+    name = 'portfolio'
--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model  # refer to user model
 
 class Investment(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="porfolio")
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="portfolio")
     portfolio_name = models.CharField(max_length=100)  # Predefined portfolio
     allocated_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
