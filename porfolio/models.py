e4d2c9282374ddd7836e4b8a677ddab11845e9fd
veronicabaquedano <baquedano2407@hotmail.com>
2025-02-03 10:35:06 +0100

    add model and serializer for porfolio

--- porfolio/models.py
+++ porfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model  # refer to user model
 
-# Create your models here.
+class Investment(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="porfolio")
+    portfolio_name = models.CharField(max_length=100)  # Predefined portfolio
+    allocated_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.portfolio_name}: ${self.allocated_amount}"
---
+++ porfolio/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Investment
+
+class InvestmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Investment
+        fields = ['id', 'user', 'portfolio_name', 'allocated_amount', 'created_at']
+        read_only_fields = ['id', 'user', 'created_at']
