eee817e67130a29dfef241020f27afe4da8231ca
veronicabaquedano <baquedano2407@hotmail.com>
2025-02-21 09:03:18 +0100

    add comments

--- frontend/src/components/Dashboard/PortfolioAllocation.jsx
+++ frontend/src/components/Dashboard/PortfolioAllocation.jsx
@@ -6,9 +6,10 @@ const PortfolioAllocation = ({ portfolio }) => {
   return (
     <div className="card p-3 mb-3">
       <h4>Portfolio Allocation</h4>
+      {/* Check if portfolio exists and is not empty */}
       {portfolio && Object.keys(portfolio).length > 0 ? (
         <>
-          {/* Display main investment types dynamically */}
+          {/* Display main investment types and their allocation percentages dynamically */}
           {Object.entries(portfolio).map(([key, value]) => (
             <p key={key}>
               <strong>{key.charAt(0).toUpperCase() + key.slice(1)}:</strong>{" "}
