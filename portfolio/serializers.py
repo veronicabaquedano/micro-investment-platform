a75ff20188dc6d725a9fe176de3736bd86bb7661
veronicabaquedano <baquedano2407@hotmail.com>
2025-02-23 10:13:31 +0100

    formatting

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import Investment
 
+
 class InvestmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Investment
-        fields = ['id', 'portfolio_name', 'allocated_amount', 'created_at']
-        read_only_fields = ['id', 'created_at']
+        fields = ["id", "portfolio_name", "allocated_amount", "created_at"]
+        read_only_fields = ["id", "created_at"]
