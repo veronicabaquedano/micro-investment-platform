62edfdae5bf39537ba6d217d98efabe55cb501b7
veronicabaquedano <baquedano2407@hotmail.com>
2025-03-02 10:48:56 +0100

    comments

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import InvestmentListCreateView, InvestmentGrowthView
 
 urlpatterns = [
-    path('', InvestmentListCreateView.as_view(), name='portfolio-list-create'),
-    path('growth/', InvestmentGrowthView.as_view(), name='investment-growth'), 
+    path("", InvestmentListCreateView.as_view(), name="portfolio-list-create"),
+    path("growth/", InvestmentGrowthView.as_view(), name="investment-growth"),
 ]
