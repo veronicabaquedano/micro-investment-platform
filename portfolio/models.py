6f5cf20d224318e7aa3445ede9118e695e77579d
veronicabaquedano <baquedano2407@hotmail.com>
2025-03-10 08:52:34 +0100

    formatting

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,14 +1,22 @@
 from django.db import models
-from django.conf import settings # refer to user model
+from django.conf import settings  # refer to user model
+
 
 class Investment(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="portfolio")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="portfolio"
+    )
     portfolio_name = models.CharField(max_length=100)  # Predefined portfolio
-    allocated_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    allocated_amount = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00
+    )
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
-        unique_together = ("user", "portfolio_name")  # Ensure unique portfolio names per user
-    
+        unique_together = (
+            "user",
+            "portfolio_name",
+        )  # Ensure unique portfolio names per user
+
     def __str__(self):
         return f"{self.user.email} - {self.portfolio_name}: ${self.allocated_amount}"
