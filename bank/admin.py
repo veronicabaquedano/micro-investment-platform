5fb5ff81c3ecfd30d4866d1680ec324cc95204ab
veronicabaquedano <baquedano2407@hotmail.com>
2025-03-12 12:43:20 +0100

    add admin for bank to decrypt account number

--- bank/admin.py
+++ bank/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import BankAccount
 
-# Register your models here.
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ("user", "bank_name", "last_four_digits", "created_at")
+
+    def last_four_digits(self, obj):
+        return "****" + obj.get_decrypted_account_number()[-4:]
+
+admin.site.register(BankAccount, BankAccountAdmin)
+
