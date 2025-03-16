6ea51bb0e0f7045b23345675c9186a7294da33cc
veronicabaquedano <baquedano2407@hotmail.com>
2025-03-16 09:29:42 +0100

    formatting

--- bank/urls.py
+++ bank/urls.py
@@ -3,6 +3,8 @@ from .views import BankAccountView
 
 urlpatterns = [
     path("", BankAccountView.as_view(), name="bank-account-list-create"),  # GET & POST
-    #account_id parameter used to identify specific bank account to delete.
-    path("<int:account_id>/", BankAccountView.as_view(), name="bank-account-delete"),  # DELETE
+    # account_id parameter used to identify specific bank account to delete.
+    path(
+        "<int:account_id>/", BankAccountView.as_view(), name="bank-account-delete"
+    ),  # DELETE
 ]
