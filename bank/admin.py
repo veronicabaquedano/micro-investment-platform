a782c2f348e2ffe42bd3f34cb7d258761e5199ee
veronicabaquedano <baquedano2407@hotmail.com>
2025-03-27 08:02:59 +0100

    formatting

--- bank/admin.py
+++ bank/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from .models import BankAccount
 
+
 class BankAccountAdmin(admin.ModelAdmin):
     list_display = ("user", "bank_name", "last_four_digits", "created_at")
 
     def last_four_digits(self, obj):
         return "****" + obj.get_decrypted_account_number()[-4:]
 
-admin.site.register(BankAccount, BankAccountAdmin)
 
+admin.site.register(BankAccount, BankAccountAdmin)
--- bank/apps.py
+++ bank/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BankConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bank'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bank"
