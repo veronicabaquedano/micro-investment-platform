989a2a8782badf438c70aafe31dc19834e63efae
veronicabaquedano <baquedano2407@hotmail.com>
2025-04-06 07:18:21 +0200

    format urls for transactions

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from transactions.views import TransactionView
 
 urlpatterns = [
-    path('', TransactionView.as_view(), name="transaction-list-create"),  # No "transactions/" here, as it's already included in the main urls.py
+    path(
+        "", TransactionView.as_view(), name="transaction-list-create"
+    ),  # No "transactions/" here, as it's already included in the main urls.py
 ]
