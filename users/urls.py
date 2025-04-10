2a8f27bf3a157d4167565a3d0b18fa6707eb71e2
veronicabaquedano <baquedano2407@hotmail.com>
2025-04-10 12:21:07 +0200

    format urls for users

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from users.views import RegisterView, LoginView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),  # Registration endpoint
-    path('login/', LoginView.as_view(), name='login'),          # Login endpoint
+    path("register/", RegisterView.as_view(), name="register"),  # Registration endpoint
+    path("login/", LoginView.as_view(), name="login"),  # Login endpoint
 ]
