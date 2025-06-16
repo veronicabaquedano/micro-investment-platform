adfba65a94f038df874ecb2efff632ab837fc472
veronicabaquedano <baquedano2407@hotmail.com>
2025-06-16 08:38:12 -0400

    add a theme for the UI

---
+++ frontend/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3de375", // Your custom green
+    },
+    // ...other palette settings
+  },
+  // ...other theme settings
+});
+
+export default theme;
+//
