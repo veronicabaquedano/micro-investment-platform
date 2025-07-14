2d6ee117c45892da518f3a4f87e60360337e64f2
veronicabaquedano <baquedano2407@hotmail.com>
2025-07-14 15:59:15 -0400

    add authentication backend in settings

--- backend/micro_investment_platform/settings.py
+++ backend/micro_investment_platform/settings.py
@@ -144,6 +144,7 @@ AUTH_USER_MODEL = "users.User"
 
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 REST_FRAMEWORK = {
